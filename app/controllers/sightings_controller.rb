aac7e305a0d9ada17bdee23829a427808e47b3e0
Laura Messa Travin <laura.travin@gmail.com>
2021-04-26 23:20:21 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,9 @@
 class SightingsController < ApplicationController
+  def index
+    sightings = Sighting.all
+    render json: SightingSerializer.new(sightings)
+  end
+
   def show
     sighting = Sighting.find(params[:id])
     # render json: sighting.to_json(:include => {:bird => {:only =>[:name, :species]}, :location => {:only =>[:latitude, :longitude]}},
